f1304cc3ac0f944726d4d8c3ed0422d6380501d1
Yasin Yuşa Küçük <yusayasin@gmail.com>
2023-10-07 19:50:07 +0300

    Add project files.

---
+++ MaviUcakInterviewProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaviUcakInterviewProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
